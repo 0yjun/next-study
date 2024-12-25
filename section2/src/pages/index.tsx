a88c8998b70183ebe7fef24db4ab6315f7d144cc
jun <an0yjun@naver.com>
2024-12-26 02:07:11 +0900

    seo설정

--- section2/src/pages/index.tsx
+++ section2/src/pages/index.tsx
@@ -5,6 +5,7 @@ import BookItem from "./components/book-item";
 import fetchBooks from "@/lib/fetch-books";
 import fetchRandomBooks from "@/lib/fetch-random-books";
 import { InferGetStaticPropsType } from "next";
+import Head from "next/head";
 
 export const getStaticProps = async () => {
   console.log("index page");
@@ -27,20 +28,28 @@ export default function Home({
   recoBooks,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
-    <div className={style.container}>
-      <section>
-        <h2>지금 추천하는 도서</h2>
-        {recoBooks.map((book) => (
-          <BookItem key={book.id} {...book} />
-        ))}
-      </section>
-      <section>
-        <h3>등록된 모든 도서</h3>
-        {allBooks.map((book) => (
-          <BookItem key={book.id} {...book} />
-        ))}
-      </section>
-    </div>
+    <>
+      <Head>
+        <title>한입북스</title>
+        <meta property="og:image" content="/thumbnail.png" />
+        <meta property="og:title" content="한입북스" />
+        <meta property="og:description" content="한입북스 설명" />
+      </Head>
+      <div className={style.container}>
+        <section>
+          <h2>지금 추천하는 도서</h2>
+          {recoBooks.map((book) => (
+            <BookItem key={book.id} {...book} />
+          ))}
+        </section>
+        <section>
+          <h3>등록된 모든 도서</h3>
+          {allBooks.map((book) => (
+            <BookItem key={book.id} {...book} />
+          ))}
+        </section>
+      </div>
+    </>
   );
 }
 
