c934242ce78a13c55bebfa83754a0c17d4ceb1c7
jun <an0yjun@naver.com>
2024-12-28 21:12:01 +0900

    데이터 패칭 적용

--- section4/src/app/(with-searchbar)/page.tsx
+++ section4/src/app/(with-searchbar)/page.tsx
@@ -1,21 +1,54 @@
 import BookItem from "@/components/book-item";
 import style from "./page.module.css";
 import books from "@/mock/books.json";
+import { BookData } from "@/types";
 
-export default function Home() {
+async function RecomandBooks(): Promise<React.ReactElement> {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`,
+    { next: { revalidate: 3 } }
+  );
+  if (!response.ok) {
+    return <div>오류가 발생했습니다.</div>;
+  }
+  const allBooks: BookData[] = await response.json();
+  return (
+    <div>
+      {allBooks.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
+}
+
+async function AllBooks(): Promise<React.ReactElement> {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`,
+    { cache: "no-store" }
+  );
+  if (!response.ok) {
+    return <div>오류가 발생했습니다.</div>;
+  }
+  const allBooks: BookData[] = await response.json();
+  return (
+    <div>
+      {allBooks.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
+}
+
+export default async function Home() {
   return (
     <div className={style.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
-        {books.map((book) => (
-          <BookItem key={book.id} {...book} />
-        ))}
+        <RecomandBooks />
       </section>
       <section>
         <h3>등록된 모든 도서</h3>
-        {books.map((book) => (
-          <BookItem key={book.id} {...book} />
-        ))}
+        <AllBooks />
       </section>
     </div>
   );
--- section4/src/app/layout.tsx
+++ section4/src/app/layout.tsx
@@ -1,8 +1,26 @@
 import "./globals.css";
 import Link from "next/link";
 import style from "./layout.module.css";
+import { BookData } from "@/types";
 
-export default function RootLayout({
+async function Footer(): Promise<React.ReactElement> {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`
+  );
+  if (!response.ok) {
+    return <footer>made by jun</footer>;
+  }
+  const books: BookData[] = await response.json();
+  const bookCount = books.length;
+  return (
+    <footer>
+      <div>made by jun {"\n"}</div>
+      <div>{bookCount}개의 도서가 등록되었습니다</div>
+    </footer>
+  );
+}
+
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
@@ -15,7 +33,7 @@ export default function RootLayout({
             <Link href={"/"}>📚 ONEBITE BOOKS</Link>
           </header>
           <main>{children}</main>
-          <footer>제작 @winterlood</footer>
+          <Footer />
         </div>
       </body>
     </html>
